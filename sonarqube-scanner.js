413e8bb46c71b00b8f4018d7f37c2025e23ddc3d
Shoaib Ahmed <sahmed12@wsgc.com>
2022-06-29 18:36:38 +0500

    docker-compose file added, sonarqube modified

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -2,7 +2,7 @@ const scanner = require("sonarqube-scanner");
 scanner(
   {
     serverUrl: "http://localhost:9000",
-    token: "sqp_bc58e0b9561c339fb607d514f27b5324bc99d348", // generated from sonarqube account
+    token: "squ_e72abf9bb11d2da7f2e3d09b2f303fec64ddd1a1", // generated from sonarqube account
     options: {
       "sonar.sources": "./src",
       "sonar.exclusions": "**/*.test.tsx",
